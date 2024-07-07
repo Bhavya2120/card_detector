0432545b218898acc4d9b81da1e7ea2111945a6d
Bhavya Dua <78466683+Bhavya2120@users.noreply.github.com>
2024-07-07 22:02:52 +0530

    Add files via upload

---
+++ card_detection/card_detect.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    # frame = cv2.resize(frame,(540,380))
+    # cv2.imshow('Frame', frame)
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    edges = cv2.Canny(blur, 50, 150)
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        approx = cv2.approxPolyDP(contour, 0.02 * cv2.arcLength(contour, True), True)
+        if len(approx) == 4:
+            # Step 4: Warp Perspective
+            pts = approx.reshape(4, 2)
+            rect = np.zeros((4, 2), dtype="float32")
+
+            s = pts.sum(axis=1)
+            rect[0] = pts[np.argmin(s)]
+            rect[2] = pts[np.argmax(s)]
+
+            diff = np.diff(pts, axis=1)
+            rect[1] = pts[np.argmin(diff)]
+            rect[3] = pts[np.argmax(diff)]
+
+            (tl, tr, br, bl) = rect
+            widthA = np.linalg.norm(br - bl)
+            widthB = np.linalg.norm(tr - tl)
+            maxWidth = max(int(widthA), int(widthB))
+
+            heightA = np.linalg.norm(tr - br)
+            heightB = np.linalg.norm(tl - bl)
+            maxHeight = max(int(heightA), int(heightB))
+
+            dst = np.array([
+                [0, 0],
+                [maxWidth - 1, 0],
+                [maxWidth - 1, maxHeight - 1],
+                [0, maxHeight - 1]], dtype="float32")
+
+            M = cv2.getPerspectiveTransform(rect, dst)
+            warp = cv2.warpPerspective(frame, M, (maxWidth, maxHeight))
+
+            cv2.imshow('Card', warp)
+
+    cv2.drawContours(frame, [approx], -1, (0, 255, 0), 2)
+    cv2.imshow('Frame', frame)
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):  # Press 'q' to exit
+        break
+cap.release()
+cv2.destroyAllWindows()
